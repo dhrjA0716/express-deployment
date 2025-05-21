ceadd4b9e48cca57329209b3a237919a3e45040c
Dheeraj Meghwal <meghwaldheeraj39@gmail.com>
2025-05-21 19:31:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+
+var app = express();
+const port = process.env.PORT || 8000;
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+var items = []
+
+var example="working";
+app.get('/', function(req, res) {
+    res.render("list",{ejes : items});
+}); 
+
+app.post('/', function(req, res) {
+    var item = req.body.ele1;
+    items.push(item);
+    res.redirect('/');
+});
+
+
+
+
+
+app.listen(8000, function() {
+    console.log("Server started");
+    });
